ca8f46c5b9ea36ec1f4a6c8aec7a8b6a646f2ce7
Maks3502 <107855225+Maks3502@users.noreply.github.com>
2023-03-30 18:39:32 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+// 1 завдання
+const str = "Привіт";
+console.log(typeof str);
+let flag = 123;
+console.log(typeof flag);
+// 2 завдання
+let yourName = prompt("Ведіть імя");
+console.log(yourName);
+// 3 завдання
+const myAge = 14;
+console.log(myAge);
+const friendAge = 13;
+console.log(friendAge)
+const resultAge = myAge + friendAge;
+alert(resultAge);
+// 4 завдання
+let  group = "FE1y_1 менші";
+const content = "Я навчаюся в GoITeens в групі:";
+console.log(content, group);
